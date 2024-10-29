47deb7de14ada3b2bd697b2aa41b06279d424dbd
paramathman <selvas19@hotmail.com>
2024-10-28 18:18:57 -0700

    Testing 123

--- api/src/exam-environment/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export { examEnvironmentPostExamAttempt } from './exam-attempt';
-export { examEnvironmentPostExamGeneratedExam } from './exam-generated-exam';
-export { examEnvironmentPostScreenshot } from './screenshot';
-export { examEnvironmentTokenVerify } from './token-verify';
-export { examEnvironmentExams } from './exams';
